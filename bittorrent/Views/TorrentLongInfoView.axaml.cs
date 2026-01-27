324e6e24836700aa40efa77907c8641d62d16d14
Jakub Novák <novak17@spsejecna.cz>
2026-01-27 21:09:43 +0100

    improve ui

---
+++ bittorrent/Views/TorrentLongInfoView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace bittorrent.Views;
+
+public partial class TorrentLongInfoView : UserControl
+{
+    public TorrentLongInfoView()
+    {
+        InitializeComponent();
+    }
+}
